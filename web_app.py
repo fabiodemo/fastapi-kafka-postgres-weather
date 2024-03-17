bf604446bd05365755dbfa2ef9bf9caf3618ea3f
fabiodemo <faberdemo@gmail.com>
2024-03-17 13:53:45 -0300

    Web app added

---
+++ web_app.py
@@ -0,0 +1,23 @@
+import os
+import json
+import psycopg2
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
+
+
+@app.get('/')
+def read_root():
+    conn = psycopg2.connect(host=POSTGRES_HOST, user=POSTGRES_USER,
+                            password=POSTGRES_PASSWORD, dbname=POSTGRES_DB)
+    cur = conn.cursor()
+    cur.execute("SELECT data FROM weather ORDER BY id DESC LIMIT 10")
+    weather_data = [json.load(row[0]) for row in cur.fetchall()]
+    cur.close()
+    conn.close()
+    return {"weather_data": weather_data}
